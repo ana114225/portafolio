fe72c3020ffefa1de1e93f72ea3a4575e7075cfa
ana114225 <123135102+ana114225@users.noreply.github.com>
2023-05-28 21:21:38 -0400

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const asunto = document.getElementById("asunto");
+const mensage = document.getElementById("mensage");
+const enviar = document.getElementById("enviar")
+const form = document.getElementById("form");
